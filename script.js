d14d00b830de1a935a45bbe4147cd2b92a712bf1
AMRUTH <136634860+AMRUTH7KRISH@users.noreply.github.com>
2025-08-03 23:33:00 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+// Vanta.js 3D background
+VANTA.NET({
+  el: "#background",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  color: 0x00ffff,
+  backgroundColor: 0x0d0d0d,
+  points: 15.0,
+  maxDistance: 25.0,
+  spacing: 15.0
+});
